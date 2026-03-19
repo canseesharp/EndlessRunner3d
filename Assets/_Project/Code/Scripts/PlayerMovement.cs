c0ae17d1c57833963f73bd193273e649c508ca87
canseesharp <hounder@ukr.net>
2026-03-19 16:37:06 +0200

    Added jump logic to player movement

--- Assets/_Project/Code/Scripts/PlayerMovement.cs
+++ Assets/_Project/Code/Scripts/PlayerMovement.cs
@@ -1,15 +1,18 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private AnimationCurve _yAnimation;
     [SerializeField] private float _shiftSpeed;
     [SerializeField] private float _fallSpeed;
 
-    private Line _line = Line.Middle;
-    private CharacterController _characterController;
     private readonly Dictionary<Line, float> _linesPosition = new();
+    private CharacterController _characterController;
+    private Line _line = Line.Middle;
+    private bool _isJumping = false;
 
     public bool TryShiftLeft()
     {
@@ -33,6 +36,36 @@ public class PlayerMovement : MonoBehaviour
         return true;
     }
 
+    public bool TryJump(Transform jumper, float duration, float height)
+    {
+        if (_characterController.isGrounded == true && _isJumping == false)
+        {
+            StartCoroutine(AnimationByTime(jumper, duration, height));
+            return true;
+        }
+
+        return false;
+    }
+
+    private IEnumerator AnimationByTime(Transform jumper, float duration, float height)
+    {
+        _isJumping = true;
+        float elapsedSeconds = 0;
+        float progress = 0;
+
+        Vector3 startPosition = jumper.position;
+
+        while (progress < 1)
+        {
+            elapsedSeconds += Time.deltaTime;
+            progress = elapsedSeconds / duration;
+            jumper.position = startPosition + new Vector3(0, _yAnimation.Evaluate(progress) * height, 0);
+
+            yield return null;
+        }
+        _isJumping = false;
+    }
+
     private void Awake()
     {
         _characterController = GetComponent<CharacterController>();
@@ -45,6 +78,9 @@ public class PlayerMovement : MonoBehaviour
     {
         float x = Mathf.Lerp(transform.position.x, _linesPosition[_line], _shiftSpeed * Time.deltaTime);
         _characterController.Move(Vector3.right * (x - transform.position.x));
-        _characterController.Move(Vector3.down * (_fallSpeed * Time.deltaTime));
+        if (_isJumping == false)
+        {
+            _characterController.Move(Vector3.down * (_fallSpeed * Time.deltaTime));
+        }
     }
 }
