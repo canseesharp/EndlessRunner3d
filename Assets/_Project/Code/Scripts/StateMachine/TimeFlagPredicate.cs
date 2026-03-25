cc1589c121e946e25f41f127a76e56ab8a2b0ca5
canseesharp <hounder@ukr.net>
2026-03-25 16:19:13 +0200

    Created predicate which represents flag that can be raised on defined time

---
+++ Assets/_Project/Code/Scripts/StateMachine/TimeFlagPredicate.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class TimeFlagPredicate : IPredicate
+{
+    private readonly float _time;
+
+    private float _setTime;
+    private bool _flag;
+
+    public TimeFlagPredicate(float time)
+    {
+        _time = time;
+    }
+
+    public void SetFlag()
+    {
+        _flag = true;
+        _setTime = Time.time;
+    }
+
+    public bool Evaluate()
+    {
+        if (_flag == true && Time.time - _setTime <= _time)
+        {
+            _flag = false;
+            return true;
+        }
+
+        return false;
+    }
+}
