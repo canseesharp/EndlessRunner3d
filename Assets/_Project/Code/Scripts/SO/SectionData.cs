78ea1e84f767a21cdedbea60e1a88b2b2a0d4290
canseesharp <hounder@ukr.net>
2026-03-28 16:25:10 +0200

    Created world moving section logic, created section data

---
+++ Assets/_Project/Code/Scripts/SO/SectionData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SectionData", menuName = "Scriptable Objects/SectionData")]
+public class SectionData : ScriptableObject
+{
+    [SerializeField] private float _speed;
+
+    public float Speed => _speed;
+}
